11c3f06319361c70aa1da11596fe03e1d584fe05
Jaylan Snelson <jaylan210@gmail.com>
2019-04-30 16:20:12 -0400

    Update cookieRunDown.js

--- cookieRunDown.js
+++ cookieRunDown.js
@@ -1,43 +1,42 @@
 <script>
   	jQuery(document).ready(function() {
-  		      //Setting The Value Of The Cookie
-      var this_cookies_value = "_j_s_4/25/2019";
-      var current_web_url = window.location.href;
+  		 
+  		//Setting The Value Of The TSI Cookie
+        var this_cookies_value = "_j_s_04/25/2019";
 
-      //Checks page on load to see if this_cookies_value already exists
-      function checkForOurCookiesValue() {
-      var allTheCookies = document.cookie;
-      var _this_Host_Name_ = '"' + window.location.hostname + '"';
-      var _this_Path_Name = window.location.pathname;
+		//Checks page on load to see if this_cookies_value already exists
+		function checkForOurCookiesValue() {
+    	var allTheCookies = document.cookie;
 
-      console.log(allTheCookies);
+    	console.log(allTheCookies);
 
-      if(allTheCookies.includes("_my_domain_cookie")) {
-        jQuery("#modal_popup").hide();
-      } else {
-        console.log("No Cookie Set")
-      };
+    	if(allTheCookies.includes(this_cookies_value)) {
+            jQuery("#modal_popup").hide();
+    	} else {
+            jQuery("#modal_popup").show();
+    	};
 
-      }
-      checkForOurCookiesValue();
+		}
+		checkForOurCookiesValue();
 
 
-      //If cookie does not exist, this script will run once users age is verified correctly
-      function createCookie(name,value,days) {
+		//If cookie does not exist, this script will run once user agrees
+		function createCookie(name,value,days) {
 
-      if (days) {
+    	if (days) {
         var date = new Date();
         date.setTime(date.getTime()+(days*24*60*60*1000));
         var expires = "; expires="+date.toGMTString();
-        }
-        else var expires = "";
-        document.cookie = name+"="+value+expires+"; path=/";
-        return true;
-        }
-      //Creating the cookie
-      jQuery(".css-only-modal-btn").on("click", function() {
-          createCookie("_my_domain_cookie", this_cookies_value, 1);
-            console.log("Cookie Set");
-      });
+   		}
+    	else var expires = "";
+    	document.cookie = name+"="+value+expires+"; path=/";
+    	return true;
+		}
+		//Creating the cookie
+		jQuery("#continue-to-site-btn").on("click", function() {
+            createCookie("_my_domain_", this_cookies_value, 1);
+            jQuery("#modal_popup").hide();
+		  	console.log("Cookie Set");
+		});
 });
-</script> 
+</script>
